88d6e64bc2a419a604e2476994d1a04204ef0212
ruanw <ruanw@10.0.0.6>
2019-11-03 13:00:07 -0200

    commit inicial pessoa

--- src/modelos/Pessoa.java
+++ src/modelos/Pessoa.java
@@ -11,4 +11,68 @@ package modelos;
  */
 public class Pessoa {
     
+    private int id;
+    private String nome;
+    private String cpf;
+    private String data_nascimento;
+    private String telefone;
+    private int id_bairro;
+    private int id_escolaridade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getId_bairro() {
+        return id_bairro;
+    }
+
+    public void setId_bairro(int id_bairro) {
+        this.id_bairro = id_bairro;
+    }
+
+    public int getId_escolaridade() {
+        return id_escolaridade;
+    }
+
+    public void setId_escolaridade(int id_escolaridade) {
+        this.id_escolaridade = id_escolaridade;
+    }
+    
 }
