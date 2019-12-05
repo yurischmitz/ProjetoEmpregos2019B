a21c417a2f5ad9eb9f8f6db7cd9d47e52ebf5d74
yuris <yuris@Yuri-Pc>
2019-12-05 20:39:23 -0200

    ÚLTIMO COMMIT!!! TRABALHO APRESENTADO

--- src/controles/Pessoa_VagaControle.java
+++ src/controles/Pessoa_VagaControle.java
@@ -99,7 +99,7 @@ public class Pessoa_VagaControle {
             SQL = " SELECT pv.id as codigo, p.nome as pessoa, e.nome as empresa, c.nome as cargo ";
             SQL += " FROM pessoas p,  cargos c, empresas e, pessoas_vagas pv, cargo_empresa ce ";
             SQL += " WHERE p.id = pv.id_pessoa AND ce.id = pv.id_cargo_empresa AND ";
-            SQL += " ce.id_cargo = c.id AND ce.id_empresa = e.id AND pv.data_exclusao is null ";
+            SQL += " ce.id_cargo = c.id AND ce.id_empresa = e.id AND pv.data_exclusao is null AND p.data_exclusao IS NULL ";
             SQL += " ORDER BY p.nome, e.nome ";
             
             result = Conexao.stmt.executeQuery(SQL);
@@ -196,7 +196,7 @@ public class Pessoa_VagaControle {
             SQL += " FROM pessoas p,  cargos c, empresas e, pessoas_vagas pv, cargo_empresa ce ";
             SQL += " WHERE p.id = pv.id_pessoa AND ce.id = pv.id_cargo_empresa AND ";
             SQL += " ce.id_cargo = c.id AND ce.id_empresa = e.id AND p.id = '" + idmostrarpessoa + "'";
-            SQL += " AND pv.data_exclusao is null ";
+            SQL += " AND pv.data_exclusao is null AND p.data_exclusao IS NULL";
             SQL += " ORDER BY e.nome ";
             
             result = Conexao.stmt.executeQuery(SQL);
